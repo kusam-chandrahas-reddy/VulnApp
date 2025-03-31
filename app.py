c053bfe0e39173e779c84d36291b5d09a59b11f4
kusam-chandrahas-reddy <kusamchandra@gmail.com>
2025-03-31 21:44:37 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from django.shortcuts import render
+from flask import Flask, render_template
 app =Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'My First page'
+    return render_template('index.html')
 
 if __name__=='__main__':
     app.run(host='0.0.0.0', debug=True, port=5500)
