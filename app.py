8a6adf25fef3d906ef20778eb6655c7a77ec810d
kusam-chandrahas-reddy <kusamchandra@gmail.com>
2025-05-05 22:38:49 +0530

    new commit adding nav

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def close_connection(exception):
 def init_db():
     with app.app_context():
         db = get_db()
-        with app.open_resource('schema.sql', mode='r') as f:
+        with app.open_resource('db_schema.sql', mode='r') as f:
             db.cursor().executescript(f.read())
         db.commit()
 
