64e131f8b47341244ad5ba9f5d1ef0cb5e5320a1
kusam-chandrahas-reddy <kusamchandra@gmail.com>
2025-05-12 22:00:28 +0530

    added cors

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app =Flask(__name__)
 app.secret_key=b'mypowerfulsecretkey'
 
 cors = CORS(app, resources={
-    r"/changepwd": {"origins": "*", "allow_credentials": True}
+    r"/changepwd": {"origins": "*", "supports_credentials": True}
 })
 
 #########################
